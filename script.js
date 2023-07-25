641e54f7a217845522c7dd47b6121c48bf0c5d22
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-07-25 13:59:24 +0000

    Complete class implementation

--- script.js
+++ script.js
@@ -22,61 +22,75 @@ const DisplayController = (() => {
     function closeModal () {
         modal.style.display = 'none';
     }
-    })();
 
 
-// Book Constructor
-class Book {
-    constructor (title, author, pages, hasRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        hasRead == true ? this.hasRead = 'Read' : this.hasRead = 'Not Read';
+    function clearDisplay () {
+        while (mainContainer.firstChild) {
+            mainContainer.removeChild(mainContainer.lastChild);
+        }
     }
 
-    toggleRead () {
-        this.hasRead == 'Read' ? this.hasRead = 'Not Read' : this.hasRead = 'Read';
-        clearDisplay();
-        updateDisplay();
+    
+    function updateDisplay () {
+        let noBooksMessage = document.querySelector('.no-books-container');
+        if (myLibrary.length == 0) {
+            mainContainer.style.display = 'none';
+            noBooksMessage.style.display = 'flex';
+        } else {
+            noBooksMessage.style.display = 'none';
+            mainContainer.style.display = 'grid';
+            for (let i = 0; i < myLibrary.length; i++) {
+                let cardDiv = document.createElement('div');
+                cardDiv.classList.add('book-card')
+                let bookTitle = document.createElement('p');
+                bookTitle.classList.add('book-title');
+                let bookAuthor = document.createElement('p');
+                bookAuthor.classList.add('book-author', 'book-content');
+                let bookPages = document.createElement('p');
+                bookPages.classList.add('book-pages', 'book-content');
+                let bookHasRead = document.createElement('p');
+                bookHasRead.classList.add('book-has-read', 'book-content');
+                let removeBttn = document.createElement('button');
+                removeBttn.textContent = 'Remove Book';
+                removeBttn.setAttribute('data', i);
+                removeBttn.addEventListener('click', () => removeBook());
+                let toggleReadBttn = document.createElement('button');
+                toggleReadBttn.textContent = 'Change read status';
+                toggleReadBttn.addEventListener('click', () => myLibrary[i].toggleRead());
+    
+                let detailsDiv = document.createElement('div');
+                detailsDiv.classList.add('details-div');
+
+                function removeBook () {
+                    let indexToRemove = removeBttn.getAttribute('data');
+                    myLibrary.splice(indexToRemove, 1);
+                    DisplayController.clearDisplay();
+                    DisplayController.updateDisplay();
+                }    
+                
+                mainContainer.appendChild(cardDiv);
+    
+                bookTitle.textContent = myLibrary[i].title;
+                bookAuthor.textContent = myLibrary[i].author;
+                bookPages.textContent = myLibrary[i].pages;
+                bookHasRead.textContent = myLibrary[i].hasRead;
+                cardDiv.appendChild(bookTitle);
+                cardDiv.appendChild(detailsDiv);
+                detailsDiv.appendChild(bookAuthor);
+                detailsDiv.appendChild(bookPages);
+                detailsDiv.appendChild(bookHasRead);
+                detailsDiv.appendChild(toggleReadBttn);
+                detailsDiv.appendChild(removeBttn);
+            }
+        }
     }
 
-    addBookToLibrary (book) {
-        clearDisplay();
-        myLibrary.push(book);
-        updateDisplay();
-    }
-}
+    
 
-const Functionality = (() => {
-
+    return {openModal, closeModal, clearDisplay, updateDisplay}
 })();
 
 
-// BEGIN NEW CODE
-
-//Modal pop-up
-let showModalBttn = document.querySelector('.open-modal-bttn');
-let closeModalBttn = document.querySelector('.close');
-let modal = document.querySelector('.modal-container')
-
-showModalBttn.addEventListener('click', openModal);
-closeModalBttn.addEventListener('click', closeModal);
-window.addEventListener('click', (event) => {
-    if (event.target == modal) {
-        closeModal()
-    }
-});
-
-function openModal () {
-    modal.style.display = 'flex';
-}
-
-function closeModal () {
-    modal.style.display = 'none';
-}
-
-
-
 // Book Constructor
 class Book {
     constructor (title, author, pages, hasRead) {
@@ -88,97 +102,20 @@ class Book {
 
     toggleRead () {
         this.hasRead == 'Read' ? this.hasRead = 'Not Read' : this.hasRead = 'Read';
-        clearDisplay();
-        updateDisplay();
+        DisplayController.clearDisplay();
+        DisplayController.updateDisplay();
     }
-}
-// function Book (title, author, pages, hasRead) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     if (hasRead == true) {
-//         this.hasRead = 'Read'
-//     } else {
-//         this.hasRead = 'Not Read'
-//     }
-// }
 
-// Book.prototype.toggleRead = function () {
-//     if (this.hasRead == 'Read') {
-//         this.hasRead = 'Not Read';
-//     } else {
-//         this.hasRead = 'Read';
-//     }
-//     clearDisplay();
-//     updateDisplay();
-// }
-
-let mainContainer = document.querySelector('.main-content');
-let myLibrary = [];
-
-function addBookToLibrary (book) {
-    clearDisplay();
-    myLibrary.push(book);
-    updateDisplay();
-}
-
-
-function clearDisplay () {
-    while (mainContainer.firstChild) {
-        mainContainer.removeChild(mainContainer.lastChild);
+    addBookToLibrary () {
+        DisplayController.clearDisplay();
+        myLibrary.push(this);
+        DisplayController.updateDisplay();
     }
+
+    
 }
 
-    //Update the display as soon as the window loads-- show stored books or show no books.
-window.addEventListener('load', updateDisplay);
-
-function updateDisplay () {
-    let noBooksMessage = document.querySelector('.no-books-container');
-    if (myLibrary.length == 0) {
-        mainContainer.style.display = 'none';
-        noBooksMessage.style.display = 'flex';
-    } else {
-        noBooksMessage.style.display = 'none';
-        mainContainer.style.display = 'grid';
-        for (let i = 0; i < myLibrary.length; i++) {
-            let cardDiv = document.createElement('div');
-            cardDiv.classList.add('book-card')
-            let bookTitle = document.createElement('p');
-            bookTitle.classList.add('book-title');
-            let bookAuthor = document.createElement('p');
-            bookAuthor.classList.add('book-author', 'book-content');
-            let bookPages = document.createElement('p');
-            bookPages.classList.add('book-pages', 'book-content');
-            let bookHasRead = document.createElement('p');
-            bookHasRead.classList.add('book-has-read', 'book-content');
-            let removeBttn = document.createElement('button');
-            removeBttn.textContent = 'Remove Book';
-            removeBttn.setAttribute('data', i);
-            removeBttn.addEventListener('click', removeBook);
-            let toggleReadBttn = document.createElement('button');
-            toggleReadBttn.textContent = 'Change read status';
-            toggleReadBttn.addEventListener('click', () => myLibrary[i].toggleRead());
-
-            let detailsDiv = document.createElement('div');
-            detailsDiv.classList.add('details-div');
-
-            
-            mainContainer.appendChild(cardDiv);
-
-            bookTitle.textContent = myLibrary[i].title;
-            bookAuthor.textContent = myLibrary[i].author;
-            bookPages.textContent = myLibrary[i].pages;
-            bookHasRead.textContent = myLibrary[i].hasRead;
-            cardDiv.appendChild(bookTitle);
-            cardDiv.appendChild(detailsDiv);
-            detailsDiv.appendChild(bookAuthor);
-            detailsDiv.appendChild(bookPages);
-            detailsDiv.appendChild(bookHasRead);
-            detailsDiv.appendChild(toggleReadBttn);
-            detailsDiv.appendChild(removeBttn);
-        }
-    }
-}
+window.addEventListener('load', DisplayController.updateDisplay);
 
 
 let form = document.forms['add-book']
@@ -186,14 +123,6 @@ form.addEventListener('submit', submitForm);
 function submitForm (event) {
     event.preventDefault();
     let book = new Book (this.title.value, this.author.value, this.pages.value, this.hasRead.checked);
-    closeModal();
-    addBookToLibrary(book);
-}
-
-
-function removeBook () {
-    let indexToRemove = this.getAttribute('data');
-    myLibrary.splice(indexToRemove, 1);
-    clearDisplay();
-    updateDisplay();
+    DisplayController.closeModal();
+    book.addBookToLibrary();
 }
